30a9a84c9b3962d07d0788c43e5f6ea6dce73d77
darzwaitz <darzwaitz@yahoo.com>
2025-03-24 22:48:58 +0000

    added an api call function - types not set

--- simple-app/src/App.tsx
+++ simple-app/src/App.tsx
@@ -1,11 +1,34 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
-function App() {
+export default function App() {
+  const [quote, setQuote] = useState();
+  const [count, setCount] = useState(0);
+
+  async function getQuote() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    // console.log(data.slip.advice);
+    // return data;
+    setQuote(data.slip.advice);
+    setCount((c) => c + 1);
+  }
+
+  useEffect(function () {
+    getQuote();
+  }, []);
+
   return (
     <div>
-      <p>Homepage</p>
+      <h1>quotePage</h1>
+      <button onClick={getQuote}>Get quote</button>
+      <p style={{ height: "16px" }}>{quote}</p>
+
+      <p style={{ height: "16px" }}>
+        {count === 0
+          ? "No quotes yet"
+          : `${count === 1 ? `${count} quote` : `${count} quotes`}`}
+      </p>
     </div>
   );
 }
-
-export default App;
